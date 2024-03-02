06194673a6fafc0f53d11ba0e35d6e648432397d
anti-duhring <mateus@alternativepayments.io>
2024-03-02 18:01:06 -0300

    adding DB_PORT env

--- internal/db/db.go
+++ internal/db/db.go
@@ -10,8 +10,9 @@ import (
 )
 
 func Init() (*sql.DB, error) {
-	connStr := fmt.Sprintf("host=%s port=5432 user=%s password=%s dbname=%s sslmode=disable",
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
 		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_NAME"),
